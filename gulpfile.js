e6240003c48dbe76b47098f6c5f0225ca2da2a20
Pedro <pedro.resch@gmail.com>
2017-12-11 23:52:16 -0200

    Added gulpfile, test styles, installed a bunch of gulp and postcss packages

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
+
+gulp.task('default', function(){
+  console.log("Hooray- you created a gulp task")
+});
+
+gulp.task('html', function (){
+  console.log("Imagine something useful being done with you html here.")
+});
+
+gulp.task('styles', function (){
+return gulp.src('./app/assets/styles/style.css')
+  .pipe(postcss([nested, cssvars, autoprefixer]))
+  .pipe(gulp.dest('./app/temp/styles'));
+
+});
+
+gulp.task('watch', function(){
+
+watch('./app/index.html', function() {
+  gulp.start('html');
+});
+
+watch('./app/assets/styles/**/*.css', function() {
+  gulp.start('styles');
+});
+
+});

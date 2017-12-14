cddf0511671ee5e07d0cc86c2d9bf38708a1521f
Pedro <pedro.resch@gmail.com>
2017-12-13 23:12:27 -0200

    Added BEM css file separation, updated gulpfile for import, updated some HTML layout, did large-header and button css

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function(){
   console.log("Hooray- you created a gulp task")
@@ -15,7 +16,7 @@ gulp.task('html', function (){
 
 gulp.task('styles', function (){
 return gulp.src('./app/assets/styles/style.css')
-  .pipe(postcss([nested, cssvars, autoprefixer]))
+  .pipe(postcss([cssImport, nested, cssvars, autoprefixer]))
   .pipe(gulp.dest('./app/temp/styles'));
 
 });
